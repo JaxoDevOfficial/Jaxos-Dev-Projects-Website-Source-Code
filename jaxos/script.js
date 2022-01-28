ed56614984b845fc97da11babb8125262caf52da
Jaxo <87024826+JaxoDevOfficial@users.noreply.github.com>
2022-01-28 18:31:01 -0500

    Update script.js

--- jaxos/script.js
+++ jaxos/script.js
@@ -2,5 +2,5 @@ function download() {
   window.open("https://github.com/jaxodevofficial/jaxos")
 }
 function demo() {
-  window.open("https://replit.com/@jaxodev/jaxos?ver=1&embed=1")
+  window.open("https://replit.com/@jaxodev/jaxos-main?embed=1")
 }
